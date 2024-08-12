0e2d2dc09e0b89dea6a3aa560ea489769fb3bcc6
Franciscosantos01 <franciscosantosaraoz2000@gmail.com>
2024-08-12 17:36:56 -0300

    Delete cv file to avoid problems with vercel

--- src/main/components/Contact.jsx
+++ src/main/components/Contact.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { socials } from '../../constants'
-import Download from '../../assets/download.png'
 import {motion} from 'framer-motion'
-import CV from '../../assets/FranciscoSantosCv.pdf'
 
 export const Contact = () => {
   return (
@@ -17,13 +15,6 @@ export const Contact = () => {
               <h2 className='font-bold'>{item.name}</h2>
             </motion.div>
           ))}
-          <motion.div whileHover={{scale: 1.1, boxShadow:"0px 0px 8px rgb(255,255,255)"}}  
-          className='w-32 h-32 rounded-full bg-white flex flex-col justify-center items-center'>
-          <a href={CV} target='_blank' download>
-          <img src={Download} alt="" className='h-16 w-16 object-contain' />
-          </a>
-              <h2>CV</h2>
-          </motion.div>
         </div>
         <motion.h3 initial={{opacity:0}} animate={{opacity:1}} transition={{delay:0.5, duration:1.2}} 
         className='text-center font-bold text-white  text-4xl sm:text-6xl mt-10 sm:p-0 p-3'>Develop by Francisco Santos</motion.h3>
